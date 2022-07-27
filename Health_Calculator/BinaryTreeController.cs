6d1dfd395473567c77610e028d95e9c493dc5641
Anatolii Kogan <a.kogan@gooligames.com>
2022-07-27 11:37:12 +0300

    Бинарное дерево максимальных элементов

---
+++ Health_Calculator/BinaryTreeController.cs
@@ -0,0 +1,167 @@
+﻿/*
+ * author: Anatolii Kogan
+ * e-mail: kogan.1anatoli@gmail.com
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Health_Calculator
+{
+    public class BinaryTreeController
+    {
+        public int Length { get; private set; } = 0;
+        private BinaryTreeElement _topElement { get; set; } = null;
+        private BinaryTreeElement _minElement { get; set; } = null;
+
+        private readonly int _maxLength = 10;
+
+        public BinaryTreeController(int maxLength)
+        {
+            _maxLength = maxLength;
+        }
+
+        public void Add(int value, int index)
+        {
+            if (_topElement == null)
+            {
+                _topElement = new BinaryTreeElement(value, null, index);
+                _minElement = _topElement;
+            }
+
+            if (Length == _maxLength)
+            {
+                //если меньше или равно, то не имеет смысла учитывать его среди итак максимальных чисел
+                if (value <= _minElement.value)
+                {
+                    return;
+                }
+                else
+                {
+                    RemoveMinElement();
+                }
+            }
+
+            Length++;
+
+            _topElement.Add(value, index);
+        }
+
+        public void RemoveMinElement()
+        {
+            if (_minElement.parent != null)
+            {
+                if (_minElement.right == null)
+                {
+                    _minElement = _minElement.parent;
+                    _minElement.left = null;
+                }
+                else
+                {
+                    var newMin = _minElement.right;
+                    _minElement.right = null;
+
+                    var tempParent = _minElement.parent;
+                    tempParent.left = newMin;
+
+                    _minElement = newMin;
+                    //все ссылки на прошлый минимальном элементе убраны
+                }
+            }
+            else
+            {
+                _topElement = _topElement.right;
+                _topElement.parent = null;
+                //ссылки на предыдущий верхний элемент убраны
+            }
+
+            Length--;
+        }
+
+        private List<BinaryTreeElement> ToList()
+        {
+            List<BinaryTreeElement> elements = new List<BinaryTreeElement>(Length);
+            elements.Add(_topElement);
+            _topElement.AddChildrenToList(elements);
+
+            return elements;
+        }
+
+        public int[] GetTreeElements()
+        {
+            int[] elements = new int[Length];
+            var treeElements = ToList();
+
+            for (int i = 0; i < Length; i++)
+            {
+                elements[i] = treeElements[i].value;
+            }
+
+            return elements;
+        }
+
+        public int[] GetTreeElementsIndexes()
+        {
+            int[] elements = new int[Length];
+            var treeElements = ToList();
+
+            for (int i = 0; i < Length; i++)
+            {
+                elements[i] = treeElements[i].index;
+            }
+
+            return elements;
+        }
+    }
+
+    public class BinaryTreeElement
+    {
+        public BinaryTreeElement parent, left, right;
+        public int value;
+        public int index;
+
+        public BinaryTreeElement(int value, BinaryTreeElement parent, int index)
+        {
+            this.value = value;
+            this.parent = parent;
+            this.index = index;
+        }
+
+        public void Add(int value, int index)
+        {
+            if (value.CompareTo(this.value) < 0)
+            {
+                if (this.left == null)
+                {
+                    this.left = new BinaryTreeElement(value, this, index);
+                }
+                else if (this.left != null)
+                    this.left.Add(value, index);
+            }
+            else
+            {
+                if (this.right == null)
+                {
+                    this.right = new BinaryTreeElement(value, this, index);
+                }
+                else if (this.right != null)
+                    this.right.Add(value, index);
+            }
+        }
+
+        public void AddChildrenToList(List<BinaryTreeElement> elements)
+        {
+            if (right != null)
+            {
+                elements.Add(right);
+                right.AddChildrenToList(elements);
+            }
+
+            if (left != null)
+            {
+                elements.Add(left);
+                left.AddChildrenToList(elements);
+            }
+        }
+    }
+}
--- Health_Calculator/MaxValuesGetter.cs
+++ Health_Calculator/MaxValuesGetter.cs
@@ -9,89 +9,26 @@ namespace Health_Calculator
     {
         public static int[] GetMaxValues(int[] array, int getValues)
         {
-            int[] maxValues = new int[getValues];
+            var tree = new BinaryTreeController(getValues);
 
-            int minValueIndex = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                if (maxValues[minValueIndex] < array[i])
-                {
-                    maxValues[minValueIndex] = array[i];
-                    SetMinValueIndexO_N_divide_4(maxValues, ref minValueIndex);
-                }
+                tree.Add(array[i], i);
             }
 
-            return maxValues;
+            return tree.GetTreeElements();
         }
 
-        /// <param name="offset">you can take, that array's indexes start from 0, into account</param>
-        public static int[] GetMaxValuesIndexes(int[] array, int getValues, int offset = 0)
+        public static int[] GetMaxValuesIndexes(int[] array, int getValues, int offset = 1)
         {
-            int[] maxValues = new int[getValues];
-            int[] maxValuesIndexes = new int[getValues];
+            var tree = new BinaryTreeController(getValues);
 
-            int minValueIndex = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                if (maxValues[minValueIndex] < array[i])
-                {
-                    maxValues[minValueIndex] = array[i];
-                    maxValuesIndexes[minValueIndex] = i + offset;
-                    SetMinValueIndexO_N_divide_4(maxValues, ref minValueIndex);
-                }
+                tree.Add(array[i], i + offset);
             }
 
-            return maxValuesIndexes;            
-        }
-
-        //O(array.Length / 4)
-        private static void SetMinValueIndexO_N_divide_4(int[] array, ref int minValueIndex)
-        {
-            int leftIndex = array.Length - 1;
-            int rightIndex = 0;
-
-            int middleIndexLeft = leftIndex + (rightIndex - leftIndex) / 2;
-            int middleIndexRight = middleIndexLeft;
-
-            int minIndexLeft = leftIndex;
-            int minIndexRight = rightIndex;
-
-            while (leftIndex >= middleIndexLeft)
-            {
-                minIndexLeft = array[leftIndex] < array[middleIndexLeft] ? leftIndex : middleIndexLeft;
-                leftIndex--;
-                middleIndexLeft++;
-
-                minIndexRight = array[rightIndex] < array[middleIndexRight] ? rightIndex : middleIndexRight;
-                rightIndex++;
-                middleIndexRight--;
-            }
-
-            minValueIndex = array[minIndexLeft] < array[minIndexRight] ? minIndexLeft : minIndexRight;
-        }
-
-        //O(array.Length / 2)
-        private static void SetMinValueIndexO_N_divide_2(int[] array, ref int minValueIndex)
-        {
-            int leftIndex = array.Length - 1;
-            int rightIndex = 0;
-
-            int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
-
-            while (leftIndex >= middleIndex)
-            {
-                if (array[leftIndex] < array[minValueIndex])
-                {
-                    minValueIndex = leftIndex;
-                }
-                if (array[rightIndex] < array[minValueIndex])
-                {
-                    minValueIndex = rightIndex;
-                }
-
-                leftIndex--;
-                rightIndex++;
-            }
+            return tree.GetTreeElementsIndexes();
         }
     }
 }
